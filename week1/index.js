5077b12499849528b74fea32748ee98fc481a52b
Haixiang <haixiang6123@gmail.com>
2019-10-01 14:53:40 -0700

    Update test cases order

--- week1/index.js
+++ week1/index.js
@@ -5,6 +5,28 @@ const config = require('../config')
 const SEPARATOR = /[ \t\n,.'":;_\-!@#$%^&*()<>\/\\{}\[\]+=|~`?]+/
 const UTF8 = 'utf8'
 
+// Function to get term frequency from a given article
+function termFrequency() {
+    const stopWords = new Set(fs.readFileSync(path.join('../', config.STOP_WORD_FILE), UTF8).split(','))
+    const article = fs.readFileSync(path.join('../', config.PRIDE_AND_PREJUDICE_FILE), UTF8)
+    const words = article.split(SEPARATOR).map(word => word.toLowerCase())
+
+    let frequency = {}
+    words.forEach(word => {
+        if (!stopWords.has(word)) {
+            if (!frequency[word]) {
+                frequency[word] = 0
+            }
+            frequency[word] += 1
+        }
+    })
+
+    return frequency
+}
+
+// Test
+const results = termFrequency()
+
 // Test cases
 const TEST_CASES = {
     mr: 786,
@@ -33,28 +55,6 @@ const TEST_CASES = {
     time: 203,
     good: 201
 }
-
-// Function to get term frequency from a given article
-function termFrequency() {
-    const stopWords = new Set(fs.readFileSync(path.join('../', config.STOP_WORD_FILE), UTF8).split(','))
-    const article = fs.readFileSync(path.join('../', config.PRIDE_AND_PREJUDICE_FILE), UTF8)
-    const words = article.split(SEPARATOR).map(word => word.toLowerCase())
-
-    let frequency = {}
-    words.forEach(word => {
-        if (!stopWords.has(word)) {
-            if (!frequency[word]) {
-                frequency[word] = 0
-            }
-            frequency[word] += 1
-        }
-    })
-
-    return frequency
-}
-
-// Test
-const results = termFrequency()
 
 for (let [word, frequency] of Object.entries(TEST_CASES)) {
     console.log(`Current: ${word} Result: ${results[word]} Actual: ${frequency}`);
