9ce91370c2a8915fa48fbca2cf812b574b35d3b8
Haixiang <haixiang6123@gmail.com>
2019-10-01 14:52:22 -0700

    Solve week1

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    STOP_WORD_FILE: 'stop_words.txt',
+    PRIDE_AND_PREJUDICE_FILE: 'pride-and-prejudice.txt'
+}
---
+++ week1/index.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+const path = require('path')
+const config = require('../config')
+
+const SEPARATOR = /[ \t\n,.'":;_\-!@#$%^&*()<>\/\\{}\[\]+=|~`?]+/
+const UTF8 = 'utf8'
+
+// Test cases
+const TEST_CASES = {
+    mr: 786,
+    elizabeth: 635,
+    very: 488,
+    darcy: 418,
+    such: 395,
+    mrs: 343,
+    much: 329,
+    more: 327,
+    bennet: 323,
+    bingley: 306,
+    jane: 295,
+    miss: 283,
+    one: 275,
+    know: 239,
+    before: 229,
+    herself: 227,
+    though: 226,
+    well: 224,
+    never: 220,
+    sister: 218,
+    soon: 216,
+    think: 211,
+    now: 209,
+    time: 203,
+    good: 201
+}
+
+// Function to get term frequency from a given article
+function termFrequency() {
+    const stopWords = new Set(fs.readFileSync(path.join('../', config.STOP_WORD_FILE), UTF8).split(','))
+    const article = fs.readFileSync(path.join('../', config.PRIDE_AND_PREJUDICE_FILE), UTF8)
+    const words = article.split(SEPARATOR).map(word => word.toLowerCase())
+
+    let frequency = {}
+    words.forEach(word => {
+        if (!stopWords.has(word)) {
+            if (!frequency[word]) {
+                frequency[word] = 0
+            }
+            frequency[word] += 1
+        }
+    })
+
+    return frequency
+}
+
+// Test
+const results = termFrequency()
+
+for (let [word, frequency] of Object.entries(TEST_CASES)) {
+    console.log(`Current: ${word} Result: ${results[word]} Actual: ${frequency}`);
+}
