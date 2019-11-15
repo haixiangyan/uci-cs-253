b3ad7f8fa046ee25de71511e8ccb046ae309eb03
Haixiang <haixiang6123@gmail.com>
2019-11-14 16:52:43 -0800

    Add 26.1

---
+++ week7/26.1.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+
+const UTF8 = 'utf8'
+
+let allWords = [[], null]
+let stopWords = [[], null]
+
+let nonStopWords = [[], () => {
+    return allWords[0].filter(word => !stopWords[0].has(word))
+}]
+let uniqueWords = [[], () => {
+    return nonStopWords[0]
+}]
+let counts = [[], () => {
+    let frequency = {}
+    uniqueWords[0].forEach(word => {
+        if (!frequency[word]) {
+            frequency[word] = 0
+        }
+        frequency[word] += 1
+    })
+    return frequency
+}]
+let sortedData = [[], () => {
+    let rawCounts = []
+    for (let [word, frequency] of Object.entries(counts[0])) {
+        rawCounts.push({word, frequency})
+    }
+
+    return rawCounts.sort((a, b) => b.frequency - a.frequency)
+}]
+
+const allColumns = [allWords, stopWords, nonStopWords, uniqueWords, counts, sortedData]
+
+function update() {
+    for (let c of allColumns) {
+        if (c[1]) {
+            c[0] = c[1]()
+        }
+    }
+}
+
+const stopWordsFile = '../stop_words.txt'
+const articleFile = '../pride-and-prejudice.txt'
+
+const pattern = /[a-zA-Z]{2,}/g
+allWords[0] = fs.readFileSync(articleFile, UTF8).match(pattern).map(word => word.toLowerCase())
+stopWords[0] = new Set(
+    fs.readFileSync(stopWordsFile, UTF8)
+        .split(',')
+        .map(stopWord => stopWord.replace('\n', ''))
+)
+
+update()
+
+for (let i = 0; i < 25; i++) {
+    console.log(sortedData[0][i].word, '-', sortedData[0][i].frequency)
+}
